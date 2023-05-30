2c1e68bd59b1cacf60bcd7aebad1b5f5cb6ff2b7
sanjai-r <samsanjai777@gmail.com>
2023-05-30 10:11:03 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,56 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React, { useState } from "react";
+
+import { btnData } from "./data/btnData";
+import Button from "./components/button";
+
+
+
+function App() {
+  const [display, setDisplay] = useState("Please press button");
+
+  function updateDisplay(token) {
+    if (display === "Please press button") {
+      setDisplay(token);
+    } else {
+      setDisplay(display + token);
+    }
+  }
+
+  function calculate() {
+    console.log("Hello, I calculate for you");
+    const results = eval(display);
+    setDisplay(results);
+  }
+
+  function handleButtonClick(token) {
+    if (token === "=") {
+      calculate();
+    } else if (token === "C") {
+      setDisplay("Please press button");
+    } else {
+      updateDisplay(token);
+    }
+  }
+
+  return (
+    <section className="w-screen h-screen justify-center items-center flex flex-col">
+      <div className="w-1/2 p-5  bg-gray-200 rounded shadow">
+        <h1 className="text-center sm:text-3xl font-bold">Calculator</h1>
+        <div className="bg-white p-2 my-4">{display}</div>
+        <div className="grid grid-cols-4 gap-2 ">
+          {btnData.map((token, index) => (
+            <Button
+              key={index}
+              onClick={() => handleButtonClick(token)}
+              extraClass="text-lg font-semibold"
+            >
+              {token}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export default App;
--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
-};
+function Button({ onClick, children, extraClass }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 sm:px-6 grid place-items-center sm:py-3 bg-blue-700 text-white sm:text-lg sm:w-24 md:w-32 rounded-md shadow-md ${extraClass}`}
+    >
+      <p>{children}</p>
+    </button>
+  );
+}
 
 export default Button;
